f4dc717e3120207bba75e9cfe65e638cedee5344
Mateusz <mateusz4k@outlook.com>
2023-10-05 17:20:56 +0200

    feat(hero): create styles for hero section - adjust also for mobile devices

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,21 @@
 module.exports = {
 	content: ['./*{html,js}'],
 	theme: {
-		extend: {},
-		colors: {
-			red: '#ab303c',
-			gray: '#9c9c9c',
-			black: '#1d1d1d',
-			green: '#30ab4c',
-			darkWhite: '#f6f6f6',
-			white: '#ffff',
+		extend: {
+			colors: {
+				red: '#ab303c',
+				gray: '#9c9c9c',
+				black: '#1d1d1d',
+				green: '#30ab4c',
+				darkWhite: '#f6f6f6',
+				white: '#ffff',
+			},
+			fontSize: {
+				heading: '120px',
+				heading2: '48px',
+				heading3: '31px',
+				normal: '20px',
+			},
 		},
 	},
 
