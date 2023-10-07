8e27df4be338bae080e84dd59f1bfc7df7f0275b
Mateusz <mateusz4k@outlook.com>
2023-10-07 14:16:56 +0200

    feat: create scrollspy and change nav into fixed position instead static

---
+++ src/js/scrollspy.js
@@ -0,0 +1,17 @@
+const sections = document.querySelectorAll('[data-nav]')
+const menuLinks = document.querySelectorAll('a')
+
+const handleScrollSpy = () => {
+	const sectionItems = []
+	sections.forEach(s => {
+		if (window.scrollY <= s.offsetTop + s.offsetHeight - 122) {
+			sectionItems.push(s)
+			const currentSection = document.querySelector(`[href*="${sectionItems[0].id}"]`)
+			menuLinks.forEach(item => item.classList.remove('active'))
+			if (!currentSection) return
+			currentSection.classList.add('active')
+		}
+	})
+}
+
+window.addEventListener('scroll', handleScrollSpy)
--- src/script.js
+++ src/script.js
@@ -1,1 +1,2 @@
 import './js/products.js'
+import './js/scrollspy.js'
